f9faced5d5412f7dfa4c75ba6eb8787f32698a50
iZergiodev <155020065+iZergiodev@users.noreply.github.com>
2024-11-06 16:57:28 +0000

    add

--- src/app.js
+++ src/app.js
@@ -6,6 +6,27 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const generateExcuse = () => {
+    const randomWho = who[Math.floor(Math.random() * who.length)];
+    const randomAction = action[Math.floor(Math.random() * action.length)];
+    const randomWhat = what[Math.floor(Math.random() * what.length)];
+    const randomWhen = when[Math.floor(Math.random() * when.length)];
+
+    const querySelectorExcuse = document.querySelector("#excuse");
+
+    querySelectorExcuse.textContent = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+  };
+
+  setInterval(generateExcuse, 2000);
 };
